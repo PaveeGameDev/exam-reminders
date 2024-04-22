2e2c6c34d85c637f136e98e0d8f7ab30741be1de
Patrik Holba <Paveegamedev@gmail.com>
2024-04-22 21:59:25 +0200

    Minor design bug fixes

--- app/components/DisplayExam.tsx
+++ app/components/DisplayExam.tsx
@@ -18,7 +18,7 @@ export default async function DisplayExam({ exam, user, isFirst }: Props) {
 
   return (
     <div className="flex flex-row justify-between w-full align-middle my-2 items-center min-h-[86px]">
-      <div className="flex flex-col ml-3 mr-1">
+      <div className="flex flex-col ml-3 mr-1 max-w-56">
         <ExamHeader exam={exam} />
         <ExpandableText showButton={false} length={isFirst ? 50 : 50}>
           {bestExamNote.content}
--- app/components/DisplayExamNote.tsx
+++ app/components/DisplayExamNote.tsx
@@ -9,11 +9,11 @@ type Props = {
 export default async function DisplayExamNote({ examNote, user }: Props) {
   return (
     <div className="card max-w-96 w-full min-w-52 bg-base-200 shadow-xl border border-gray-300 flex items-center justify-center">
-      <div className="card-body text-center justify-center items-center">
+      <div className="card-body text-center justify-center items-center w-full">
         <h2 className="card-title justify-center w-full">
           {await getDisplayName(examNote, user)}
         </h2>
-        <p className="break-words">{examNote.content}</p>
+        <p className="break-words max-w-full">{examNote.content}</p>
         <div className="card-actions justify-center mt-4 w-32">
           <DisplayExamNoteButton examNote={examNote} user={user} />
         </div>
