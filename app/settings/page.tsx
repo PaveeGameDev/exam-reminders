b93ecd3aecde51f57af409e65e8c1b2b35302c49
Patrik Holba <Paveegamedev@gmail.com>
2024-05-26 20:09:38 +0200

    Notifications 28 - notifications.tsx hidden for real

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -23,11 +23,11 @@ export default async function Settings() {
   return (
     <main className="flex justify-center">
       <div className="space-y-5 w-full max-w-md mx-auto">
-        <Notifications
-          FB_API_KEY={process.env.FIREBASE_API_KEY!}
-          FB_measurement_id={process.env.FIREBASE_MEASUREMENT_ID!}
-          user={user}
-        />
+        {/*<Notifications*/}
+        {/*  FB_API_KEY={process.env.FIREBASE_API_KEY!}*/}
+        {/*  FB_measurement_id={process.env.FIREBASE_MEASUREMENT_ID!}*/}
+        {/*  user={user}*/}
+        {/*/>*/}
         <InstallPWA />
         <UserInfo user={user} />
         <JoinClass />
