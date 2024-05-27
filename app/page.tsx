d2584428f84ceac0bbdd3e4654964ac14fad5e07
Patrik Holba <Paveegamedev@gmail.com>
2024-05-27 20:37:10 +0200

    Notifications 31 - private message fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,37 +17,12 @@ export default async function Home() {
   if (!user.classId) return "Musíte se přihlásit do třídy";
   const exams = await getUpcomingExams(user);
   if (!exams) return;
-  const currentUserExamPreferences = await prisma.userExamPreferences.findMany({
-    where: { userId: user.id },
-  });
-
-  const isWanted = async (exam: Exam) =>
-    currentUserExamPreferences.some(
-      (preference) =>
-        (preference.examId === exam.id && preference.stateId === 0) ||
-        (preference.examId === exam.id && preference.stateId === null),
-    ) ||
-    currentUserExamPreferences.every(
-      (preference) => preference.examId !== exam.id,
-    );
-
-  let filteredExams = [];
-
-  for (const exam of exams) {
-    if (await isWanted(exam)) {
-      filteredExams.push(exam);
-    }
-  }
 
   const dayViewWrappers = [];
 
-  filteredExams = filteredExams.filter(
-    (exam) => !exam.followerId || exam.followerId === user.id,
-  );
-
   const date = new Date();
   for (let i = 0; i < 30; i++) {
-    const examsOnTheDay = filteredExams.filter(
+    const examsOnTheDay = exams.filter(
       (exam) =>
         exam.date.getDate() === date.getDate() &&
         exam.date.getMonth() === date.getMonth(),
--- functions/getUpcomingExams.ts
+++ functions/getUpcomingExams.ts
@@ -7,42 +7,55 @@ export const getUpcomingExams = async (
   date?: { minDate: Date; maxDate: Date },
 ): Promise<Exam[] | null> => {
   const activeSubjects = await getUserActiveSubject(user);
-  if (date) {
-    const allExams = await prisma.exam.findMany({
-      where: {
-        classId: user.classId!,
-        stateId: 0,
-        date: {
-          gte: date.minDate,
-          lte: date.maxDate,
-        },
-      },
-    });
-    return allExams.filter((exam) =>
-      activeSubjects.find((subject) => exam.subjectId === subject.id),
-    );
-  }
+  const innerDate: { minDate: Date; maxDate: Date } | undefined = date ?? {
+    minDate: new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      new Date().getDate(),
+    ),
+    maxDate: new Date(
+      new Date().getFullYear(),
+      new Date().getMonth() + 1,
+      new Date().getDate(),
+    ),
+  };
 
   const allExams = await prisma.exam.findMany({
     where: {
       classId: user.classId!,
       stateId: 0,
       date: {
-        gte: new Date(
-          new Date().getFullYear(),
-          new Date().getMonth(),
-          new Date().getDate(),
-        ),
-        lte: new Date(
-          new Date().getFullYear(),
-          new Date().getMonth() + 1,
-          new Date().getDate(),
-        ),
+        gte: innerDate.minDate,
+        lte: innerDate.maxDate,
       },
     },
   });
 
-  return allExams.filter((exam) =>
-    activeSubjects.find((subject) => exam.subjectId === subject.id),
-  );
+  const currentUserExamPreferences = await prisma.userExamPreferences.findMany({
+    where: { userId: user.id },
+  });
+
+  const isWanted = async (exam: Exam) =>
+    currentUserExamPreferences.some(
+      (preference) =>
+        (preference.examId === exam.id && preference.stateId === 0) ||
+        (preference.examId === exam.id && preference.stateId === null),
+    ) ||
+    currentUserExamPreferences.every(
+      (preference) => preference.examId !== exam.id,
+    );
+
+  const filteredExams = [];
+
+  for (const exam of allExams) {
+    if (await isWanted(exam)) {
+      filteredExams.push(exam);
+    }
+  }
+
+  return allExams
+    .filter((exam) =>
+      activeSubjects.find((subject) => exam.subjectId === subject.id),
+    )
+    .filter((exam) => !exam.followerId || exam.followerId === user.id);
 };
