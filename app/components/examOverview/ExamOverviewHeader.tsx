d45c01341282b1e4c8af4082d3bacb651068ff2d
Patrik Holba <Paveegamedev@gmail.com>
2024-06-06 22:19:01 +0200

    ExamOverview redesign 4 - bug fixes

--- app/components/examOverview/ExamOverviewHeader.tsx
+++ app/components/examOverview/ExamOverviewHeader.tsx
@@ -2,21 +2,22 @@ import { Exam } from "@prisma/client";
 import { getFancyDayName } from "@/functions/getFancyDayName";
 import { getExamTypeById } from "@/functions/getExamTypeById";
 import { getSubjectById } from "@/functions/getSubjectById";
+import { getSubjectNameById } from "@/functions/getSubjectNameById";
 
 type Props = {
   exam: Exam;
 };
 export default async function ExamOverviewHeader({ exam }: Props) {
-  const subject = await getSubjectById(exam.subjectId);
+  const subject = await getSubjectNameById(exam.subjectId);
   const type = await getExamTypeById(exam.examTypeId);
 
   return (
     <div className="flex flex-col items-center space-y-3 mb-3">
       <p className="text-4xl underline underline-offset-4">
         {getFancyDayName(exam.date)} - {exam.date.getDate()}.
-        {exam.date.getMonth()}
+        {exam.date.getMonth() + 1}
       </p>
-      <p className="text-4xl font-bold">{subject?.name}</p>
+      <p className="text-4xl font-bold">{subject}</p>
       <p className="text-3xl">
         {type?.name}
         {exam.followerId ? " (Privátní)" : ""}
